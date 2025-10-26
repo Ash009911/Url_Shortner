83b0c104be449126fa982534247e9e4e40d8fb55
Ashutosh <arceusfreefire002@gmail.com>
2025-10-26 13:48:43 +0530

    Fix header title in App component to reflect correct branding

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <div className='flex flex-col bg-[#FFF2E0] items-center justify-between h-screen text-black font-bold'>
       <div className="  flex flex-col justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold my-4 text-[#898AC4]">Affordmed Company's Own URL Shortener</h1>
+      <h1 className="text-3xl font-bold my-4 text-[#898AC4]">Ash's Own URL Shortener</h1>
       <ShortenerForm />
     </div>
 
